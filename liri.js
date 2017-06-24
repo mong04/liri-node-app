d4b7f5ace83f0b87f7f11cea70a436aeb90a7982
Andrew Sanchez <bballbizniz@aol.com>
2017-06-24 09:06:50 -0700

    pushing newest working code

--- liri.js
+++ liri.js
@@ -1,50 +1,84 @@
 const keys = require('./keys.js');
 const request = require('request');
 const inquirer = require('inquirer');
+const fs = require('fs')
 
 const client = keys.client;
 const spotify = keys.spotify;
 let input = process.argv;
 
-switch(input[2]){
+function liri(){
+	switch(input[2]){
 	case "my-tweets":
 		inquirer.prompt([
 		{
-			type: "input",
-			message: "What is your Username?",
-			name: "username"
+			type: "list",
+			message: "What tweets would you like to show?",
+			choices: ["Default", "Custom (Input Username)"],
+			name: "tweets"
 		}
-		]).then(function(answer){
-			var params = {screen_name: answer.username,
-				count: 20};
-			client.get('statuses/user_timeline', params, function(error, tweets, response) {
-			  if (!error) {
-			    for (let i = 0; i < tweets.length; i++){
-			    	console.log("\n"+ tweets[i].text +
-			    		"\nCreated at: " + tweets[i].created_at);
-			    }
-			  }
-			});
+		]).then(function(response){
+			switch(response.tweets)
+			{
+				case "Default":
+					let params = {screen_name: "JohneeTest1",
+					count: 20};
+					client.get('statuses/user_timeline', params, function(error, tweets, response) {
+						if (!error) {
+							for (let i = 0; i < tweets.length; i++){
+								console.log("\n" + tweets[i].text +
+									"\nCreated at: " + tweets[i].created_at);
+							}
+						}
+						else {
+							console.log("\nError: " + error[0].code +
+								"\n" + error[0].message);
+						}
+					})
+					break;
+				case "Custom (Input Username)":
+					inquirer.prompt([
+					{
+						type: "input",
+						message: "Whose tweets would you like to see?",
+						name: "username"
+					}
+					]).then(function(input){
+						let params = {screen_name: input.username,
+						count: 20};
+						client.get('statuses/user_timeline', params, function(error, tweets, response) {
+							if (!error) {
+								for (let i = 0; i < tweets.length; i++){
+									console.log("\n" + tweets[i].text +
+										"\nCreated at: " + tweets[i].created_at);
+								}
+							}
+							else{
+								console.log("\nError: " + error[0].code +
+									"\n" + error[0].message);
+							}
+						})
+					})
+					break;
+			}
 		})
 		break;
+
 	case "spotify-this-song":
 		let songName = "";
 		for (let i = 3; i < input.length; i++){
 			songName += input[i] + " ";
 		}
-		spotify
-		  .search({ type: 'track', query: songName })
-		  .then(function(response) {
-		  	let song = response.tracks.items[0];
-		    console.log("\nArtist Name: " + song.artists[0].name +
-		    	"\nSong Name: " + song.name +
-		    	"\nPreview Link: " + song.preview_url +
-		    	"\nAlbum: " + song.album.name);
-		  })
-		  .catch(function(err) {
-		    console.log(error);
-		  });	
-		console.log(songName);
+		spotify.search({ type: 'track', query: songName }, function(err, data) {
+		  if (err) {
+		    return console.log('Error occurred: ' + err);
+		  }
+	  	  let song = data.tracks.items[0];
+	      console.log("\nArtist Name: " + song.artists[0].name +
+	      	"\nSong Name: " + song.name +
+	      	"\nPreview Link: " + song.preview_url +
+	     	"\nAlbum: " + song.album.name); 
+		});
 		break;
 	case "movie-this":
 		let movieName = "";
@@ -68,13 +102,31 @@ switch(input[2]){
 					"\nActors: " + parse(body).Actors +
 					"\nRotten Tomatoes URL: " + rottenURL);
 			}
+			else {
+				console.log(error);
+			}
 		})
 		break;
 	case "do-what-it-says":
-		console.log("do-what-it-says");
+		fs.readFile('random.txt', 'utf-8', function(err, data){
+			if (err){
+				return console.log(err);
+			}
+
+			var output = data.split(',');
+
+			for (var i = 0; i < output.length; i++) {
+				let a = 2;
+				return input[a] = output[i];
+				console.log(input[a]);
+				a++
+			}
+		});
+		liri();
 		break;
 }
-
+};
+liri();
 // `my-tweets`
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
 // `spotify-this-song`
